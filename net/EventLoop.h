48dce29b25b860a65206e8717c282d6ed474a74f
daguai <13935474921@163.com>
2025-05-22 14:03:23 +0800

    添加EventLoop模块

--- net/EventLoop.h
+++ net/EventLoop.h
@@ -1,12 +1,68 @@
 #ifndef MUDUO_NET_EVENTLOOP_H
 #define MUDUO_NET_EVENTLOOP_H
+#include "Timestamp.h"
+#include "CurrentThread.h"
+#include <vector>
+#include <atomic>
+#include <memory>
+#include <mutex>
+#include <functional>
+
+
+class Channel;
+class Poller;
 
 class EventLoop
 {
 public:
+    using Functor = std::function<void()>;
+
+    EventLoop();
+    ~EventLoop();
+
+    // 开启事件循环
+    void loop();
+    // 退出事件循环
+    void quit();
+
+    Timestamp pollReturnTime() const { return pollReturnTime_;}
+
+    //在当前loop中执行cb
+    void runInLoop(Functor cb);
+    // 把cb放入队列中，唤醒loop所在的线程,执行cb
+    void queueInLoop(Functor cb);
+
+    // 用来唤醒loop所在的线程
+    void wakeup();
+
+    void updateChannel(Channel* channel);
+    void removeChannel(Channel* channel);
+    bool hasChannel(Channel* channel);
+
+    bool isInLoopThread() const { return threadId_ == CurrentThread::tid(); }
 
 private:
+    void handleRead(); //weked up
+    void doPendingFunctors();
 
+    using ChannelList = std::vector<Channel*>;
+
+    std::atomic_bool looping_; /*atomic*/
+    std::atomic_bool quit_;
+
+    const pid_t threadId_;
+
+    Timestamp pollReturnTime_;
+    std::unique_ptr<Poller> poller_;
+
+    int wakeupFd_;
+    std::unique_ptr<Channel> wakeupChannel_;
+
+    ChannelList activeChannels_;
+
+    std::atomic_bool callingPendingFunctors_; 
+    std::vector<Functor> pendingFunctors_; 
+    std::mutex mutex_;
 };
 #endif
 
