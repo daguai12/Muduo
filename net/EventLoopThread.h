3ee446378f639264b87c2b09a90ca8b688919461
daguai <13935474921@163.com>
2025-05-23 13:01:02 +0800

    添加EventLoopThread模块

---
+++ net/EventLoopThread.h
@@ -0,0 +1,35 @@
+#ifndef MUDUO_NET_EVENTLOOPTHREAD_H 
+#define MUDUO_NET_EVENTLOOPTHREAD_H 
+#include "EventLoop.h"
+#include "nocopyable.h"
+#include "Thread.h"
+
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+
+class EventLoop;
+
+class EventLoopThread : noncopyable
+{
+public:
+    using ThreadInitCallback = std::function<void(EventLoop*)>;
+
+    EventLoopThread(const ThreadInitCallback &cb = ThreadInitCallback(),
+                    const std::string &name = std::string());
+
+    ~EventLoopThread();
+
+    EventLoop* startLoop();
+private:
+    void threadFunc();
+
+    EventLoop *loop_;
+    bool exiting_;
+    Thread thread_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    ThreadInitCallback callback_;
+};
+#endif
